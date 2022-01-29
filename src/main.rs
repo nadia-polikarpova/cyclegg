82df2a71310acce9ebfe569067cc3031d8320ecb
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2022-01-29 10:41:17 -0800

    Refactor types out in a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,149 @@
 use egg::{*, rewrite as rw};
-use std::collections::HashMap;
+use symbolic_expressions::{Sexp, SexpError};
+use std::{collections::HashMap, str::FromStr, fmt::Display};
 
-type Type = RecExpr<SymbolLang>;
+mod types;
+use types::{*};
+
+// Both expressions and types are just SymbolLang expressions for now
 type Expr = RecExpr<SymbolLang>;
-type Eg = EGraph<SymbolLang, ()>;
 
-// Constructors is a map from datatype names to constructor names
-type Constructors = HashMap<Symbol, Vec<Symbol>>;
+
+// We will use SymbolLang e-graphs with no analysis for now
+type Eg = EGraph<SymbolLang, ()>;
+type Rw = Rewrite<SymbolLang, ()>;
+
+// Environment: for now just a map from datatype names to constructor names
+type Env = HashMap<Symbol, Vec<Symbol>>;
 
 // Type context
 type Context = HashMap<Symbol, Type>;
 
-fn case_split(
-    mut egraph: Eg,
-    e: &Expr,
-    ctx: &Context,
-    constructors: &Constructors,
-) -> (Eg, Eg) {
-  let c = egraph.lookup_expr(e).unwrap();
-  let mut esucc = egraph.clone();
-
-  let c0 = egraph.add(SymbolLang::leaf("zero"));  
-  egraph.union(c, c0);
-  egraph.rebuild();
-
-  let pat = format!("(succ x{})", egraph.total_size());
-  let c1 = esucc.add_expr(&pat.parse().unwrap());
-  esucc.union(c, c1);
-  esucc.rebuild();
-
-  (egraph, esucc)
+// Proof goal
+struct Goal<'a> {
+  // Equivalences we already proved
+  egraph: Eg,
+  // Rewrites that are valid for the current goal
+  rewrites: Vec<Rw>,
+  // Context
+  ctx: Context,
+  // Variables we haven't case-split on yet
+  scrutinees: Vec<Symbol>,
+  // Our goal is to prove lhs == rhs
+  lhs: &'a Expr,
+  rhs: &'a Expr,
+  // Environment
+  env: &'a Env,
 }
+
+impl<'a> Goal<'a> {
+  // Have we proven that lhs == rhs?
+  pub fn done(&self) -> bool {
+    let lhs_id = self.egraph.lookup_expr(self.lhs).unwrap();
+    let rhs_id = self.egraph.lookup_expr(self.rhs).unwrap();
+    lhs_id == rhs_id
+  }
+
+  // Saturate the goal by applying all available rewrites
+  pub fn saturate(mut self) -> Self {
+    let runner = Runner::default().with_egraph(self.egraph).run(self.rewrites.iter());
+    self.egraph = runner.egraph;
+    self
+  }
+
+  // Consume this goal and add its case splits to the proof state
+  pub fn case_split(mut self, state: &mut ProofState) {
+    // Get the next variable to case-split on
+    let var = self.scrutinees.pop().unwrap();
+    // Get the type of the variable
+    let ty = self.ctx.get(&var).unwrap();
+    // Get the constructors of the datatype
+    let cons = self.env.get(&var).unwrap();
+    // For each constructor,
+    for &con in cons {
+      // Get the type of the constructor
+      let con_ty = self.ctx.get(&con).unwrap();
+      // Create a new subgoal
+      // let mut new_goal = Goal {
+      //   lhs: goal.lhs.clone(),
+      //   rhs: goal.rhs.clone(),
+      //   egraph: goal.egraph.clone(),
+      //   rewrites: goal.rewrites.clone(),
+      //   ctx: goal.ctx.clone(),
+      //   scrutinees: goal.scrutinees.clone(),
+      // };
+      // Add a rewrite to the subgoal
+      // new_goal.rewrites.push(rw::CaseSplit(var, con));
+      // Add the subgoal to the proof state
+      // state.push(new_goal);
+    }
+
+  }
+}
+// A proof state is a list of subgoals,
+// all of which have to be discharged
+type ProofState<'a> = Vec<Goal<'a>>;
+
+fn proof_step(state: &mut ProofState) {
+  // Pop the first subgoal
+  let mut goal = state.pop().unwrap();
+  // Saturate the goal
+  goal = goal.saturate();
+  if goal.done() {
+    // If this goal has been discharged, we're done
+    return
+  } else {
+    // Otherwise, we need to case-split on a variable
+    goal.case_split(state)
+  }
+}
+
+// fn case_split(
+//     mut egraph: Eg,
+//     e: &Expr,
+//     ctx: &Context,
+//     env: &Env,
+// ) -> Vec<Eg> {
+//   let c = egraph.lookup_expr(e).unwrap();
+//   let mut esucc = egraph.clone();
+
+//   let c0 = egraph.add(SymbolLang::leaf("zero"));  
+//   egraph.union(c, c0);
+//   egraph.rebuild();
+
+//   let pat = format!("(succ x{})", egraph.total_size());
+//   let c1 = esucc.add_expr(&pat.parse().unwrap());
+//   esucc.union(c, c1);
+//   esucc.rebuild();
+
+//   vec![egraph, esucc]
+// }
 
 fn main() {
   let context = Context::from([
     (Symbol::from("x"), "Nat".parse().unwrap()),
     (Symbol::from("zero"), "Nat".parse().unwrap()),
-    (Symbol::from("succ"), "(-> Nat Nat)".parse().unwrap()),
-    (Symbol::from("add"), "(-> Nat (-> Nat Nat))".parse().unwrap()),
+    (Symbol::from("succ"), "(-> (Nat) Nat)".parse().unwrap()),
+    (Symbol::from("add"), "(-> (Nat Nat) Nat)".parse().unwrap()),
   ]);
 
-  let constructor = Constructors::from([
+  println!("arity of Nat: {:?}", &context.get(&Symbol::from("x")).unwrap().args());
+  println!("arity of Nat -> Nat: {:?}", &context.get(&Symbol::from("succ")).unwrap().args());
+  println!("arity of Nat -> Nat -> Nat: {:?}", &context.get(&Symbol::from("add")).unwrap().args());
+  
+
+  let constructor = Env::from([
     (Symbol::from("Nat"), vec![Symbol::from("zero"), Symbol::from("succ")]),
   ]);
 
-  let rules: &[Rewrite<SymbolLang, ()>] = &[
+  let rules: &[Rw] = &[
+    rw!("add-zero"; "(add zero ?y)" => "?y"),
+    rw!("add-succ"; "(add (succ ?x) ?y)" => "(succ (add ?x ?y))"),
+    rw!("triv-zero"; "(triv zero)" => "true"),
+    rw!("triv-succ"; "(triv (succ ?x))" => "true"),
+  ];
+
+  let rules1: Vec<Rw> = vec![
     rw!("add-zero"; "(add zero ?y)" => "?y"),
     rw!("add-succ"; "(add (succ ?x) ?y)" => "(succ (add ?x ?y))"),
     rw!("triv-zero"; "(triv zero)" => "true"),
@@ -63,17 +162,18 @@ fn main() {
   egraph.add_expr(&lhs);
   egraph.add_expr(&rhs);
 
-  let runner = Runner::default().with_egraph(egraph).run(rules);
+  // let runner = Runner::default().with_egraph(egraph).run(rules);
+  let runner = Runner::default().with_egraph(egraph).run(rules1.iter());
   println!("e-graph size: {}", runner.egraph.number_of_classes());
   println!("{} == {}", runner.egraph.lookup_expr(&lhs).unwrap(), runner.egraph.lookup_expr(&rhs).unwrap());
 
-  let scrutinee = "x".parse().unwrap();
-  let (egraph, esucc) = case_split(runner.egraph, &scrutinee, &context, &constructor);
-  let runner1 = Runner::default().with_egraph(egraph).run(rules);
-  let runner2 = Runner::default().with_egraph(esucc).run(rules);
+  // let scrutinee = "x".parse().unwrap();
+  // let (egraph, esucc) = case_split(runner.egraph, &scrutinee, &context, &constructor);
+  // let runner1 = Runner::default().with_egraph(egraph).run(rules);
+  // let runner2 = Runner::default().with_egraph(esucc).run(rules);
 
-  println!("e-graph size: {}", runner1.egraph.number_of_classes());
-  println!("{} == {}", runner1.egraph.lookup_expr(&lhs).unwrap(), runner1.egraph.lookup_expr(&rhs).unwrap());
-  println!("e-graph size: {}", runner2.egraph.number_of_classes());
-  println!("{} == {}", runner2.egraph.lookup_expr(&lhs).unwrap(), runner2.egraph.lookup_expr(&rhs).unwrap());
+  // println!("e-graph size: {}", runner1.egraph.number_of_classes());
+  // println!("{} == {}", runner1.egraph.lookup_expr(&lhs).unwrap(), runner1.egraph.lookup_expr(&rhs).unwrap());
+  // println!("e-graph size: {}", runner2.egraph.number_of_classes());
+  // println!("{} == {}", runner2.egraph.lookup_expr(&lhs).unwrap(), runner2.egraph.lookup_expr(&rhs).unwrap());
 }
---
+++ src/types.rs
@@ -0,0 +1,41 @@
+use symbolic_expressions::{Sexp, SexpError};
+use std::{str::FromStr, fmt::Display};
+
+#[derive(Debug, Clone)]
+pub struct Type { 
+  repr: Sexp
+}
+
+impl Type {
+  fn new(repr: Sexp) -> Self {
+    Self { repr }
+  }
+
+  pub fn args(&self) -> Vec<Type> {
+    match &self.repr {
+      Sexp::String(_) => vec![],  // This type is a D
+      Sexp::List(xs) => {         // This type is a (-> (D1 ... Dn) D)
+        let mut args = vec![];
+        for x in xs[1].list().unwrap() {
+          args.push(Type::new(x.clone()));
+        }
+        args        
+      }
+      _ => panic!("arity: type is empty"),
+    }
+  }
+}
+
+impl FromStr for Type {
+  type Err = SexpError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    Ok(Type::new(symbolic_expressions::parser::parse_str(s)?))
+  }
+}
+
+impl Display for Type {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "{}", self.repr)
+  }
+}
